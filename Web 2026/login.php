b757e6836bf43b1ca1e7f6b0c02d9f5c38e893c1
22247-glitch <22247@northcote.school.nz>
2026-06-29 14:22:16 +1200

    Update login.php

--- Web 2026/login.php
+++ Web 2026/login.php
@@ -1,42 +1,39 @@
 <?php
 session_start();
-include"conn.php";
-?>
+include "conn.php";
 
-<?php
-//test whether the LogIn button on this form has been hit
-  if(isset($_POST['LogIn'])){  
-	  
-//Assign variables to the Email and Password values entered on the form	  
-	$LoginUsername = $_POST['Username'];
-	$LoginPassword = $_POST['Password'];
+if(isset($_POST['Login'])){   
+    
+    $LoginUsername = $_POST['Username'];
+    $LoginPassword = $_POST['Password'];
 
-//Look for a matching record in the members table 
-//and return the records as an array
-	$result = $conn->query("select * from member 
-	where username='$LoginUsername' and password='$LoginPassword'");
-	$row = $result->fetch_array();
-	
-//Assign the email, password and user_id from the array to variables	
-  $id=$row['user_id'];
-	$Username=$row['username'];
-  $Password=$row['password'];
-	
-//Assign a Session Variable to each field we want to reuse in this session 
-	$_SESSION['UserID'] = $row['user_id'];	
-	$_SESSION["Username"] = $row['username'];
-	$_SESSION["Password"] = $row['password'];	
-//Checking the values entered on the login form against the values in the database. 
-//If they match direct the user to the members page
-    if($LoginUsername==$Username && $LoginPassword==$Password)
-    {
-?>
-    <script>window.location ="Home.html?id=<?php echo $id;?>";</script>
-<?php
+    
+    $result = $conn->query("select * from member where username='$LoginUsername' and password='$LoginPassword'");
+    
+    if ($result && $result->num_rows > 0) {
+        $row = $result->fetch_array();
+        
+        $id = $row['user_id'];
+        $Username = $row['username'];
+        $Password = $row['password'];
+        
+        $_SESSION['UserID'] = $row['user_id'];  
+        $_SESSION["Username"] = $row['username'];
+        $_SESSION["Password"] = $row['password']; 
+
+        if($LoginUsername == $Username && $LoginPassword == $Password) {
+            ?>
+            <script>window.location ="Home.html?id=<?php echo $id;?>";</script>
+            <?php
+            exit();
+        }
+    } else {
+        
+        ?>
+        <script>window.location ="Tracking.html";</script>
+        <?php
+        exit();
     }
-    else{
-    header("location: Tracking.html"); //send user back to the login page.
-	}
 }
 ?>
 
@@ -56,23 +53,21 @@ include"conn.php";
     <form id="LoginForm" name="LoginForm" method="post" action=""> 
       <div class="title">
       <h1 class="center">Login:</h1>
-      <!-- text field-->
+      
       <div class="inputs">
         <label><b>Username</b></label>
-        <input input name="Username" type="text" required="required" placeholder="Enter Username">
+        <input name="Username" type="text" required="required" placeholder="Enter Username">
       </div>
-      <!-- text field-->
+      
       <div class="inputs">
         <label><b>Password</b></label>
-        <input input name="Password" type="text" required="required" placeholder="Enter Password">
+        <input name="Password" type="password" required="required" placeholder="Enter Password">
       </div>
-      <!-- button-->
+      
       <div class="inputs">
         <input name="Login" type="submit" class="button" id="LogIn" value="LogIn">
       </div>
     </form>
-
   </div> 
-
 </body>
 </html>
